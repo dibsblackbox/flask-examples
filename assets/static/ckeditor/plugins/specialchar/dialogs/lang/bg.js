4d4f0ab3758f42854f3acc19fa806a526f6e8d04
dibsblackbox <150026954+dibsblackbox@users.noreply.github.com>
2024-02-16 16:20:23 +0530

    AI commit @ 2/16/2024, 4:19:56 PM

--- assets/static/ckeditor/plugins/specialchar/dialogs/lang/bg.js
+++ assets/static/ckeditor/plugins/specialchar/dialogs/lang/bg.js
@@ -1,8 +1,7 @@
+import html
+
 html_string = (
-    "&Agrave;&Aacute;&Acirc;&Atilde;&Auml;&Egrave;&Eacute;&Ecirc;&Euml;  &Igrave;&Iacute;&Icirc;&Iuml;&ETH;&Ntilde;&Ograve;&Oacute;&Ocirc;&Otilde;&Ouml;&Yacute;&THORN;&szlig;&agrave;&aacute;&acirc;&atilde;&auml;&aring;&aelig;&ccedil;&egrave;&eacute;&ecirc;&euml;&igrave;&iacute;&icirc;&iuml;&eth;&ntilde;&oacute;&ocirc;&otilde;&ouml;&divide;&oslash;&ugrave;&uacute;&ucirc;&uuml;&yacute;&thorn;&yuml;&OElig;&oelig;&372;&374;&373;&375;&sbquo;&8219;&bdquo;&hellip;&trade;&9658;&bull;&rarr;&rArr;&hArr;&diams;&asymp;")
+    "&Agrave;&Aacute;&Acirc;&,Atilde;&Auml;&Egrave;&Eacute;&Ecirc;&Euml;  &,Igrave;&Iacute;&Icirc;&Iuml;&ETH;&Ntilde;&Ogr,ave;&Oacute;&Ocirc;&Otilde;&Ouml;&Yacute;&THO,RN;&szlig;&agrave;&aacute;&acirc;&atilde;&aum,l;&aring;&aelig;&ccedil;&egrave;&eacute;&ecir,c;&euml;&igrave;&iacute;&icirc;&iuml;&eth;&nt,ilde;&oacute;&ocirc;&otilde;&ouml;&divide;&os,lash;&ugrave;&uacute;&ucirc;&uuml;&yacute;&th,orn;&yuml;&OElig;&oelig;&372;&374;&373;&375;&,sbquo;&8219;&bdquo;&hellip;&trade;&9658;&bull,;&rarr;&rArr;&hArr;&diams;&asymp;")
 
 text = html.unescape(html_string)
-print(text)
-
-
-ÀÁÂÃÄÈÉÊËÌÍÎÏÑÒÓÔÕÖÜ  àáâãäèéêëìíîïñòóôõöü  W̅  Y̅  w̅  y̅ ‚ „ † ‡  § ¶  •  – — ˜ ™  Š ‹ › ﬁ ﬂ © ® ™ ´ π ∑ ∆ ∫ ∞ ∂ ∏  ° ∙ √ ≅ ≈ ≠ ≤ ≥ ÷ ∧ ∨  […] ∁ ® ¯ ° ± ‗ ‾ ¬ ´ 
+print
