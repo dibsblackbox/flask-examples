d95fbd62c33d0517996dacd3ac98356a879c00b7
dibsblackbox <150026954+dibsblackbox@users.noreply.github.com>
2024-02-16 16:57:56 +0530

    AI commit @ 2/16/2024, 4:57:45 PM

--- assets/static/ckeditor/plugins/specialchar/dialogs/lang/ro.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "/api/example", true);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send(JSON.stringify({ value: "Example" }));
-
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/api/example?param1=value1&param2=value2", true);
-xhr.send();
