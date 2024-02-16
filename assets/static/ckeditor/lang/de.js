e04635ec3679ec7149e0f7d8b2ae498946c2d3c0
dibsblackbox <150026954+dibsblackbox@users.noreply.github.com>
2024-02-16 14:23:29 +0530

    AI commit @ 2/16/2024, 2:23:20 PM

--- assets/static/ckeditor/lang/de.js
+++
@@ -1,1 +0,0 @@
-Refused to apply style from 'https://cdn.ckeditor.com/4.16.1/full-all/contents.css' because its MIME type ('text/plain') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
