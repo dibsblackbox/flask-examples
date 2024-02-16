463d08eceade5b4e60cdfde43a813fbb0e710035
dibsblackbox <150026954+dibsblackbox@users.noreply.github.com>
2024-02-16 16:19:56 +0530

    AI commit @ 2/16/2024, 4:19:28 PM

--- assets/static/ckeditor/plugins/specialchar/dialogs/lang/af.js
+++ assets/static/ckeditor/plugins/specialchar/dialogs/lang/af.js
@@ -1,43 +1,43 @@
     try:
-        resp = requests.post(
-            self.API_ENDPOINT + "/verify_purchase",
-            json={
-                "in_app_purchase_data": json.dumps(in_app_purchase_data),
-                "purchase_token": purchase_token,
-                "product_id": product_id,
-                "subscription_option": subscription_option,
-            },
-            headers={
-                "X-API-Key": self.api_key,
-                "X-APP-VERSION": app_version,
-                "X-DEVICE-ID": device_id,
-                "X-PLATFORM": platform,
-                "Content-Type": "application/json",
-            },
+        headers = {
+            "X-API-Key": self.api_key,
+            "X-APP-VERSION": app_version,
+            "X-DEVICE-ID": device_id,
+            "X-PLATFORM": platform,
+            "Content-Type": "application/json",
+        }
+
+        data = {
+            "in_app_purchase_data": json.dumps(in_app_purchase_data),
+            "purchase_token": purchase_token,
+            "product_id": product_id,
+            "subscription_option": subscription_option,
+        }
+
+        response = requests.post(
+            self.API_ENDPOINT + "/verify_purchase", headers=headers, json=data
         )
 
-        resp.raise_for_status()
+        response.raise_for_status()
 
-        response_json = resp.json()
-
-        return response_json["result"]
+        return response.json()
 
     except requests.exceptions.HTTPError as errh:
-        print ("Http Error:",errh)
+        print("Http Error:", errh)
         return None
 
     except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc)
+        print("Error Connecting:", errc)
         return None
 
     except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
+        print("Timeout Error:", errt)
         return None
 
     except requests.exceptions.RequestException as err:
-        print ("Something went wrong",err)
+        print("Something went wrong", err)
         return None
 
     except Exception as e:
-        print ("Exception",str(e))
+        print("Exception:", str(e))
         return None
