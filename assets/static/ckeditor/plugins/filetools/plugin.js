c8b5e084fb4886362491e874a255840d9adf07b7
dibsblackbox <150026954+dibsblackbox@users.noreply.github.com>
2024-02-16 16:17:32 +0530

    AI commit @ 2/16/2024, 4:16:57 PM

--- assets/static/ckeditor/plugins/filetools/plugin.js
+++ assets/static/ckeditor/plugins/filetools/plugin.js
@@ -1,36 +1,21 @@
-var fileTools = CKEDITOR.plugins.fileTools;
-var fileLoader = new fileTools.FileLoader(editor, {
+// Improved Code:
+
+const fileTools = CKEDITOR.plugins.fileTools;
+const fileLoader = new fileTools.FileLoader(editor, {
 	uploadUrl: 'https://myserver.com/upload'
 });
 
-// The data will be send using form-data encoded POST request
-// (other request types like 'PUT' and 'POST' are also supported)
-fileLoader.load(new Blob([1,2,3,4], {type: 'image/png'}));
-
 // Add listener to customize headers before request
-editor.on('fileUploadRequest', function(evt) {
-	var xhr = evt.data.fileLoader.xhr;
+editor.on('fileUploadRequest', (evt) => {
+	const xhr = evt.data.fileLoader.xhr;
 
+	// Set token and CSRF
 	xhr.setRequestHeader('X-CSRF-TOKEN', 'abc');
 	xhr.setRequestHeader('Content-Type', 'text/plain');
 });
 
 // The result of upload is delivered through fileUploadResponse event
-editor.on('fileUploadResponse', function(evt) {
-	console.log('Uploaded filename: ' + evt.data.response.filename);
-	console.log('Uploaded file url: ' + evt.data.response.url);
+editor.on('fileUploadResponse', (evt) => {
+	console.log(`Uploaded filename: ${evt.data.response.filename}`);
+	console.log(`Uploaded file url: ${evt.data.response.url}`);
 });
-
-
-var fileTools = CKEDITOR.plugins.fileTools;
-var fileLoader = new fileTools.FileLoader(editor, {
-	uploadUrl: 'https://myserver.com/upload'
-});
-
-// Add listener to customize headers before request
-editor.on('fileUploadRequest', function(evt) {
-	var xhr = evt.data.fileLoader.xhr;
-
-	// Set token and CSRF
-	xhr.setRequestHeader('X-CSRF-TOKEN', 'abc');
-
